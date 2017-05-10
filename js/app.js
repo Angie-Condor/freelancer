c9b609415543d5563e7377325b0cfd990ab1b1e4
Angie Condor <angiegita1602@gmail.com>
2017-05-10 14:13:30 -0500

    portfolio y scroll ok

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 /*
  * Archivo principal de JS
  */
+
+ var endScroll = 0;
+window.addEventListener("scroll", function () {
+  var headScroll = document.getElementById("head-scroll");
+  var scroll = window.pageYOffset || document.body.scrollTop;
+  if (scroll > endScroll) {
+    headScroll.classList.add("header-style");
+  }
+  else{
+    headScroll.classList.remove("header-style");
+  }
+});
